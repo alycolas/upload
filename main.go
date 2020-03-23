3c522977e5a6d6db320aa7b82c1609f6fcc0eaa3
alycolas <alycolas@gmail.com>
2020-03-23 16:33:33 +0800

    test note.html

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"time"
 	"strconv"
@@ -11,6 +12,12 @@ import (
 	"net/http"
 //	"strings"
 	"log"
+)
+
+const (
+	noteTMPL =  "note.html"
+	uploadTMPL = "upload.gtpl"
+	noteFile = "1.db"
 )
 
 // func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -26,7 +33,28 @@ import (
 // 	fmt.Fprintf(w, "Hello World!") // 这个写入到 w 的是输出到客户端的
 // }
 
-// 处理 /upload  逻辑
+// 处理 /note 
+func note(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+//		crutime := time.Now().Unix()
+//		h := md5.New()
+//		io.WriteString(h, strconv.FormatInt(crutime, 10))
+//		token := fmt.Sprintf("%x", h.Sum(nil))
+		note, _ := ioutil.ReadFile(noteFile)
+		t, _ := template.ParseFiles("noteTMPL")
+		t.Execute(w, note)
+	} else {
+		newNote = r.FormValue("note")
+		err := ioutil.WriteFile(noteFile, []byte(newNote), 0666)
+		if err != nil {
+			fmt.Println("can not update note")
+			return
+		}
+	}
+}
+
+// 处理 /upload
 func upload(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
 	if r.Method == "GET" {
@@ -35,7 +63,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(h, strconv.FormatInt(crutime, 10))
 		token := fmt.Sprintf("%x", h.Sum(nil))
 
-		t, _ := template.ParseFiles("/var/www/upload.gtpl")
+		t, _ := template.ParseFiles("uploadTMPL")
 		t.Execute(w, token)
 	} else {
 		r.ParseMultipartForm(32 << 20)
@@ -59,6 +87,7 @@ func upload(w http.ResponseWriter, r *http.Request) {
 func main() {
 //	http.HandleFunc("/", sayhelloName)
 	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/note", note)
 	err := http.ListenAndServe("127.0.0.1:9090", nil) // 设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
